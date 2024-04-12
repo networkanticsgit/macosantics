318f43029822bd7fe5312df2c62c3e3bf61101e9
Mahir Mujkanovic <mahir.mujkanovic@outlook.com>
2024-04-12 11:00:28 -0700

    Create FileVaultAuthenticatedRemoteRestart.sh

---
+++ FileVaultAuthenticatedRemoteRestart.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RecoveryKey="ENTER RECOVERY KEY HERE"
+
+echo "$RecoveryKey"
+
+sudo fdesetup authrestart -inputplist <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+<key>Password</key>
+<string>$RecoveryKey</string>
+</dict>
+</plist>
+EOF
